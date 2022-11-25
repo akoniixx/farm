b6dfa323cd1d7cc74bf1e46ea1a273871a61383d
akon.ick <alongkon.i@iconkaset.com>
2022-11-25 14:32:12 +0700

    mixpanel prod

--- mixpanel.ts
+++ mixpanel.ts
@@ -1,9 +1,9 @@
 import {Mixpanel} from 'mixpanel-react-native';
-import { MIXPANEL_DEV } from './src/config/develop-config';
+import { MIXPANEL_DEV, MIXPANEL_PROD } from './src/config/develop-config';
 
 const trackAutomaticEvents = true;
 export const mixpanel = new Mixpanel(
-  MIXPANEL_DEV,
+  MIXPANEL_PROD,
   trackAutomaticEvents,
 );
 mixpanel.init();
