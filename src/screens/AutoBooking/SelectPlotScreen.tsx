9e7e72f5859e251ec27ea667667c69ff47e58b44
akon.ick <alongkon.i@iconkaset.com>
2023-01-17 11:48:52 +0700

    save

--- src/screens/AutoBooking/SelectPlotScreen.tsx
+++ src/screens/AutoBooking/SelectPlotScreen.tsx
@@ -7,6 +7,7 @@ import { colors, font, icons, image } from '../../assets';
 import { MainButton } from '../../components/Button/MainButton';
 import DatePickerCustom from '../../components/Calendar/Calendar';
 import CustomHeader from '../../components/CustomHeader';
+import PlotSelect from '../../components/Plots/PlotSelect';
 import PlotInProfile from '../../components/Plots/PlotsInProfile';
 import StepIndicatorHead from '../../components/StepIndicatorHead';
 import TimePicker from '../../components/TimePicker/TimePicker';
@@ -17,121 +18,152 @@ import { initProfileState, profileReducer } from '../../hook/profilefield';
 
 
 const SelectPlotScreen: React.FC<any> = ({ navigation }) => {
-    
-    const [profilestate, dispatch] = useReducer(profileReducer, initProfileState);
-    const [plotList,setPlotList] = useState<any>()
-    const [loading, setLoading] = useState(false);
 
-    const getPlotlist = async() => {
-        setLoading(true)
-        const farmer_id = await AsyncStorage.getItem('farmer_id');
-        PlotDatasource.getPlotlist(farmer_id!)
-        .then(res => {
-           setPlotList(res)
-           setTimeout(() => setLoading(false), 200);
-        })
-        .catch(err => console.log(err));
-    }
-    useEffect(() => {
-        getPlotlist()
-       
-      }, []);
+  const [profilestate, dispatch] = useReducer(profileReducer, initProfileState);
+  const [plotList, setPlotList] = useState<any>()
+  const [loading, setLoading] = useState(false);
+  const [selectedCard, setSelectedCard] = useState<number | null>(null);
+  
 
-      console.log(plotList)
-    return (
-        <>
-            <StepIndicatorHead
-                curentPosition={1}
-                onPressBack={() => navigation.goBack()}
-                label={'เลือกแปลงของคุณ'}
-            />
-            
-{plotList?.data?.length === 0 ? (
-                <View>
-                    
-                  <Image
-                    source={image.empty_plot}
-                    style={{
-                      width: normalize(138),
-                      height: normalize(120),
-                      alignSelf: 'center',
-                      top: '5%',
-                    }}
-                  />
-                  <Text
-                    style={{
-                      fontFamily: font.SarabunLight,
-                      fontSize: normalize(16),
-                      color: colors.gray,
-                      textAlign: 'center',
-                      paddingVertical: normalize(22),
-                    }}>{`คุณไม่มีแปลงเกษตร
+  const handleCardPress = (index: number) => {
+    setSelectedCard(index);
+  };
+
+
+  const getPlotlist = async () => {
+    setLoading(true)
+    const farmer_id = await AsyncStorage.getItem('farmer_id');
+    PlotDatasource.getPlotlist(farmer_id!)
+      .then(res => {
+        setPlotList(res)
+        setTimeout(() => setLoading(false), 200);
+      })
+      .catch(err => console.log(err));
+  }
+  useEffect(() => {
+    getPlotlist()
+
+  }, []);
+
+  console.log(plotList)
+  return (
+    <>
+      <StepIndicatorHead
+        curentPosition={1}
+        onPressBack={() => navigation.goBack()}
+        label={'เลือกแปลงของคุณ'}
+      />
+
+      {plotList?.data?.length === 0 ? (
+        <View>
+
+          <Image
+            source={image.empty_plot}
+            style={{
+              width: normalize(138),
+              height: normalize(120),
+              alignSelf: 'center',
+              top: '5%',
+            }}
+          />
+          <Text
+            style={{
+              fontFamily: font.SarabunLight,
+              fontSize: normalize(16),
+              color: colors.gray,
+              textAlign: 'center',
+              paddingVertical: normalize(22),
+            }}>{`คุณไม่มีแปลงเกษตร
  กดเพิ่มแปลงเกษตรได้เลย!`}</Text>
 
-                  <View style={[styles.buttonAdd]}>
-                    <Text style={styles.textaddplot}>+ เพิ่มแปลงเกษตร</Text>
-                  </View>
-                </View>
-              ) : (
-                <View style={{top: 5}}>
-                    <Text>sds</Text>
-                    {/*  {console.log(plotList?.data)} */}
-                   {plotList?.data.map((i:any)=>(
-<Text>{i.id}</Text>
+          <View style={[styles.buttonAdd]}>
+            <Text style={styles.textaddplot}>+ เพิ่มแปลงเกษตร</Text>
+          </View>
+        </View>
+      ) : (
+        <SafeAreaView edges={['bottom','left','right']} style={{ flex:1,justifyContent:'space-between' }}>
 
-                   ))}
-                  <ScrollView
-                    style={{paddingVertical: 10}}
-                    horizontal
-                    showsHorizontalScrollIndicator={false}>
-                  
-                  </ScrollView>
-                </View>
+
+
+          <ScrollView
+            style={{ paddingVertical: 10 }}
+            horizontal
+            showsHorizontalScrollIndicator={false}>
+            <FlatList
+              data={plotList?.data}
+              renderItem={({ item, index }) => (
+                <PlotSelect
+                  id={item.id}
+                  plotName={item.plotName}
+                  plantName={item.plantName}
+                  locationName={item.locationName}
+                  raiAmount={item.raiAmount}
+                  onPress={() => handleCardPress(index)}
+                  selected={index === selectedCard}
+                />
               )}
-           
-        </>
-    )
+              keyExtractor={(item) => item.id}
+
+            />
+          </ScrollView>
+          <MainButton
+            label="ถัดไป"
+            color={colors.greenLight}
+           /*  onPress={() => navigation.navigate('SelectTarget')} */
+            style={{margin:normalize(10)}}
+          />
+        </SafeAreaView>
+      )}
+
+    </>
+  )
 }
 export default SelectPlotScreen
 
 const styles = StyleSheet.create({
-    label: {
-        fontFamily: font.AnuphanMedium,
-        fontSize: normalize(20),
-        color: colors.fontBlack,
-    },
-    input: {
-        fontFamily: font.SarabunLight,
-        height: normalize(56),
-        marginVertical: 12,
-        padding: 5,
-        borderColor: colors.disable,
-        borderWidth: 1,
-        borderRadius: normalize(10),
-        color: colors.fontBlack,
-        fontSize: normalize(16),
-    },
-    h1: {
-        fontFamily: font.AnuphanBold,
-        fontSize: normalize(20),
-        color: colors.greenLight,
-    },
-    buttonAdd: {
-        borderColor: '#1F8449',
-        borderWidth: 1,
-        borderRadius: 10,
-        height: normalize(80),
-        width: normalize(350),
-        borderStyle: 'dashed',
-        position: 'relative',
-        alignSelf: 'center',
-      },
-      textaddplot: {
-        fontFamily: font.AnuphanBold,
-        fontSize: normalize(20),
-        color: colors.greenLight,
-        textAlign: 'center',
-        top: '30%',
-      },
+  label: {
+    fontFamily: font.AnuphanMedium,
+    fontSize: normalize(20),
+    color: colors.fontBlack,
+  },
+  input: {
+    fontFamily: font.SarabunLight,
+    height: normalize(56),
+    marginVertical: 12,
+    padding: 5,
+    borderColor: colors.disable,
+    borderWidth: 1,
+    borderRadius: normalize(10),
+    color: colors.fontBlack,
+    fontSize: normalize(16),
+  },
+  h1: {
+    fontFamily: font.AnuphanBold,
+    fontSize: normalize(20),
+    color: colors.greenLight,
+  },
+  buttonAdd: {
+    borderColor: '#1F8449',
+    borderWidth: 1,
+    borderRadius: 10,
+    height: normalize(80),
+    width: normalize(350),
+    borderStyle: 'dashed',
+    position: 'relative',
+    alignSelf: 'center',
+  },
+  textaddplot: {
+    fontFamily: font.AnuphanBold,
+    fontSize: normalize(20),
+    color: colors.greenLight,
+    textAlign: 'center',
+    top: '30%',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
 })
 
