be26e57af1c765145c8cb0fb9f42a5a41fdb8dc6
akon.ick <alongkon.i@iconkaset.com>
2023-01-18 09:47:46 +0700

    save

--- src/screens/AutoBooking/SelectPlotScreen.tsx
+++ src/screens/AutoBooking/SelectPlotScreen.tsx
@@ -109,7 +109,7 @@ const SelectPlotScreen: React.FC<any> = ({ navigation }) => {
           <MainButton
             label="ถัดไป"
             color={colors.greenLight}
-           /*  onPress={() => navigation.navigate('SelectTarget')} */
+            onPress={() => navigation.navigate('SelectTarget')}
             style={{margin:normalize(10)}}
           />
         </SafeAreaView>
