79720f9aa6bd0ec2a3c901e76f2f12b0e4d0fc9d
narongkorn.h.iconkaset <narongkorn.h.iconkaset@gmail.com>
2023-07-25 16:55:08 +0700

    changelog

--- android/app/src/main/java/com/farmer/MainActivity.java
+++ android/app/src/main/java/com/farmer/MainActivity.java
@@ -13,11 +13,11 @@ public class MainActivity extends ReactActivity {
         super.onCreate(null); // set null for oppo
     }
 
-   @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);  // here
-        super.onCreate(savedInstanceState);
-    }
+  //  @Override
+  //   protected void onCreate(Bundle savedInstanceState) {
+  //       SplashScreen.show(this);  // here
+  //       super.onCreate(savedInstanceState);
+  //   }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
